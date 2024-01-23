38f28fe45c1070f6de5df783036cff86ba5674e7
Kas Buunk <kasbuunk@icloud.com>
2024-01-23 20:46:13 +0100

    chore: reconfigure hop

--- init.lua
+++ init.lua
@@ -109,7 +109,7 @@ require('lazy').setup({
     'smoka7/hop.nvim',
     version = "*",
     opts = {
-      keys = 'tnpldhfucj/quzkovseriao', -- optimized for colemak-dh-jk
+      keys = 'tnpldhfucquzovseriao', -- optimized for colemak-dh-jk
     },
   },
   {
@@ -805,21 +805,8 @@ cmp.setup {
 }
 
 -- Configure hop.
--- place this in one of your configuration file(s)
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
-vim.keymap.set('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
-vim.keymap.set('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, { remap = true })
-vim.keymap.set('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
 
 -- Hop keymaps.
 vim.keymap.set('n', '<leader>hw', ':HopWord<cr>', { desc = '[H]op[W]ord' })
