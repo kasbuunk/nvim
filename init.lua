7410f6e29e21f4f94edd0c1c40e21b245284e535
Kas Buunk <kasbuunk@icloud.com>
2024-01-24 14:29:45 +0100

    fix: remove duplicate lsp autocompletion

--- init.lua
+++ init.lua
@@ -121,7 +121,6 @@ require('lazy').setup({
       'saadparwaiz1/cmp_luasnip',
 
       -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-nvim-lsp-signature-help',
